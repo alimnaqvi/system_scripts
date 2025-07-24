5734a23c0afb7ddf5f7f5cac4d59c173ced334a2
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-07-24 14:40:24 +0200

    correct name of Pop light theme in auto-dark-theme script

--- desktop_settings/GNOME/auto-dark-theme.sh
+++ desktop_settings/GNOME/auto-dark-theme.sh
@@ -3,10 +3,12 @@
 CURRENT_HOUR=$(date +%H)
 
 if [[ ${CURRENT_HOUR} -ge 6 && ${CURRENT_HOUR} -lt 20 ]]; then
-    THEME="light"
+    COLOR_SCHEME="prefer-light"
+    GTK_THEME="Pop"
 else
-    THEME="dark"
+    COLOR_SCHEME="prefer-dark"
+    GTK_THEME="Pop-dark"
 fi
 
-gsettings set org.gnome.desktop.interface color-scheme "prefer-${THEME}"
-gsettings set org.gnome.desktop.interface gtk-theme "Pop-${THEME}"
+gsettings set org.gnome.desktop.interface color-scheme "${COLOR_SCHEME}"
+gsettings set org.gnome.desktop.interface gtk-theme "${GTK_THEME}"
