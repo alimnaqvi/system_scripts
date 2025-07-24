c8df656a16462a9b8f60652f34e4aebaffa660a5
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-07-24 19:43:53 +0200

    update big-agi backup script to remove old backup files

--- backups/big-agi-regular-backup.sh
+++ backups/big-agi-regular-backup.sh
@@ -2,22 +2,31 @@
 
 DOWNLOADS_DIR=~/Downloads/
 BACKUP_DIR=~/my_projects/LLMs/big-AGI-backups/
+TO_BACK_UP=(${DOWNLOADS_DIR}backup_chats*)
 
-for FILE in ${DOWNLOADS_DIR}backup_chats*; do
+if [[ ${#TO_BACK_UP[@]} -gt 1 ]]; then
+    echo "${DOWNLOADS_DIR} contains more than one file. Please ensure there is only one file to back up"
+    exit 1
+fi
 
-    FILE_BASE=$(basename ${FILE})
+FILE=${TO_BACK_UP[0]}
 
-    # Rename file to add current hostname for better identification
-    FILE_BASE_HOST=$(echo "${FILE_BASE}" | sed "s/backup_chats/backup_chats_$(uname -n)/g") \
-    && echo "Renamed ${FILE_BASE} to ${FILE_BASE_HOST}"
+FILE_BASE=$(basename ${FILE})
 
-    if [[ -f "${BACKUP_DIR}${FILE_BASE_HOST}" ]]; then
-        echo "Skipping file ${FILE_BASE_HOST} because it already exists in ${BACKUP_DIR}."
-        continue
-    fi
+# Rename file to add current hostname for better identification
+FILE_BASE_HOST=$(echo "${FILE_BASE}" | sed "s/backup_chats/backup_chats_$(uname -n)/g")
 
-    echo "Attempting to copy ${FILE} to ${BACKUP_DIR}${FILE_BASE_HOST}"
-    cp ${FILE} "${BACKUP_DIR}${FILE_BASE_HOST}"
+if [[ -f "${BACKUP_DIR}${FILE_BASE_HOST}" ]]; then
+    echo "Skipping file ${FILE_BASE_HOST} because it already exists in ${BACKUP_DIR}"
+    exit
+fi
 
-done \
+OLD_FILES=(${BACKUP_DIR}backup_chats*)
+if [[ -f ${OLD_FILES[0]} ]]; then
+    echo "Deleting previous backups in ${BACKUP_DIR}"
+    rm "${BACKUP_DIR}backup_chats*"
+fi
+
+echo "Attempting to copy ${FILE} to ${BACKUP_DIR}${FILE_BASE_HOST}"
+cp ${FILE} "${BACKUP_DIR}${FILE_BASE_HOST}" \
 && cd ${BACKUP_DIR} && git add . && git commit -m "regular backup" && git push && echo "Successfully finished big-agi-regular-backup"
